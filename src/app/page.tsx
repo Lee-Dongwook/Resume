6658e912aeeb0d55983f03554312cb14344c91ee
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-17 19:00:31 +0900

    fix: UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,51 +11,51 @@ const BLUR_FADE_DELAY = 0.04;
 
 export default function page() {
   return (
-    <main className="flex flex-col mt-28 min-h-[100vh] space-y-10">
-      <section id="hero">
-        <div className="mx-auto w-full max-w-2xl space-y-8">
-          <div className="gap-2 flex justify-between">
-            <div className="flex-col flex flex-1 space-y-20">
-              <BlurFadeText
-                delay={BLUR_FADE_DELAY}
-                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
-                yOffset={8}
-                text={`Hi, I'm ${Data.name} 👋`}
-              />
-              <BlurFade delay={BLUR_FADE_DELAY}>
-                <BlurFadeText
-                  className="max-w-full md:text-3xl"
-                  delay={BLUR_FADE_DELAY}
-                  text={Data.description}
-                />
-              </BlurFade>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section id="about">
-        <div className="flex items-center justify-center">
-          <BlurFade delay={BLUR_FADE_DELAY * 3}>
-            <h2 className="text-xl font-bold">About</h2>
-          </BlurFade>
-          <BlurFade delay={BLUR_FADE_DELAY * 4}>
-            <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert"></Markdown>
+    <main className="flex flex-col justify-center items-center mt-16 md:mt-28 min-h-screen space-y-10 px-4 sm:px-8 md:px-12 lg:px-20">
+      {/* Hero Section */}
+      <section id="hero" className="w-full max-w-3xl mx-auto">
+        <div className="flex flex-col items-center space-y-10">
+          <BlurFadeText
+            delay={BLUR_FADE_DELAY}
+            className="text-3xl font-bold tracking-tight sm:text-5xl xl:text-6xl"
+            yOffset={8}
+            text={`Hi, I'm ${Data.name} 👋`}
+          />
+          <BlurFade delay={BLUR_FADE_DELAY}>
+            <BlurFadeText
+              className="text-lg md:text-2xl text-muted-foreground"
+              delay={BLUR_FADE_DELAY}
+              text={Data.description}
+            />
           </BlurFade>
         </div>
       </section>
-      <section id="work">
-        <div className="flex min-h-0 flex-col justify-center items-center gap-y-3">
-          <BlurFade delay={BLUR_FADE_DELAY * 5}>
-            <h2 className="text-xl font-bold">Work Experience</h2>
-          </BlurFade>
+
+      {/* About Section */}
+      <section id="about" className="text-center">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-4xl font-bold">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full mx-auto text-3xl font-sans text-muted-foreground dark:prose-invert">
+            {Data.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+
+      {/* Work Section */}
+      <section id="work" className="w-full max-w-3xl mx-auto space-y-6">
+        <BlurFade delay={BLUR_FADE_DELAY * 5}>
+          <h2 className="text-4xl font-bold text-center">Work Experience</h2>
+        </BlurFade>
+        <div className="flex flex-col space-y-4">
           {Data.work.map((work, id) => (
             <BlurFade
               key={work.company}
               delay={BLUR_FADE_DELAY * 6 + id * 0.05}
             >
               <ResumeCard
-                key={work.company}
-                altText={work.company}
+                company={work.company}
                 title={work.title}
                 badges={work.badges}
                 period={`${work.start} - ${work.end ?? "Current"}`}
@@ -65,18 +65,19 @@ export default function page() {
           ))}
         </div>
       </section>
-      <section id="education">
-        <div className="min-h-0 flex items-center justify-center flex-col gap-y-3">
-          <BlurFade delay={BLUR_FADE_DELAY * 7}>
-            <h2 className="text-xl font-bold">Education</h2>
-          </BlurFade>
+
+      {/* Education Section */}
+      <section id="education" className="w-full max-w-3xl mx-auto space-y-6">
+        <BlurFade delay={BLUR_FADE_DELAY * 7}>
+          <h2 className="text-4xl font-bold text-center">Education</h2>
+        </BlurFade>
+        <div className="flex flex-col space-y-4">
           {Data.education.map((education, id) => (
             <BlurFade
               key={education.school}
               delay={BLUR_FADE_DELAY * 8 + id * 0.05}
             >
               <ResumeCard
-                key={education.school}
                 altText={education.school}
                 title={education.school}
                 period={`${education.start} - ${education.end}`}
@@ -85,64 +86,66 @@ export default function page() {
           ))}
         </div>
       </section>
-      <section id="skills">
-        <div className="min-h-0 flex items-center justify-center flex-col gap-y-3">
-          <BlurFade delay={BLUR_FADE_DELAY * 9}>
-            <h2 className="text-xl font-bold">Skills</h2>
-          </BlurFade>
-          <div className="flex flex-wrap gap-1">
-            {Data.skills.map((skill, id) => (
-              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
-                <Badge key={skill}>{skill}</Badge>
-              </BlurFade>
-            ))}
-          </div>
+
+      {/* Skills Section */}
+      <section id="skills" className="w-full max-w-3xl mx-auto space-y-6">
+        <BlurFade delay={BLUR_FADE_DELAY * 9}>
+          <h2 className="text-4xl font-bold text-center">Skills</h2>
+        </BlurFade>
+        <div className="flex flex-wrap gap-5 justify-center">
+          {Data.skills.map((skill, id) => (
+            <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+              <Badge>{skill}</Badge>
+            </BlurFade>
+          ))}
         </div>
       </section>
-      <section id="projects">
-        <div className="space-y-12 w-full py-12">
-          <BlurFade delay={BLUR_FADE_DELAY * 11}>
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
-              <div className="space-y-2">
-                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  My Projects
-                </div>
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Check out my latest work
-                </h2>
-                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  I&apos;ve worked on a variety of projects, from simple
-                  websites to complex web applications. Here are a few of my
-                  favorites.
-                </p>
-              </div>
+
+      {/* Projects Section */}
+      <section id="projects" className="w-full max-w-3xl mx-auto py-12">
+        <BlurFade delay={BLUR_FADE_DELAY * 11}>
+          <div className="text-center">
+            <div className="inline-block bg-foreground text-background px-3 py-1 text-sm rounded-lg">
+              My Projects
             </div>
-          </BlurFade>
-          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
-            {Data.projects.map((project, id) => (
-              <BlurFade
-                key={project.title}
-                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
-              >
-                <></>
-              </BlurFade>
-            ))}
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl mt-4">
+              Check out my latest work
+            </h2>
+            <p className="text-muted-foreground text-lg mt-4">
+              I&apos;ve worked on a variety of projects, from simple websites to
+              complex web applications.
+            </p>
           </div>
+        </BlurFade>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-8">
+          {Data.projects.map((project, id) => (
+            <BlurFade
+              key={project.title}
+              delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+            >
+              <div className="bg-white p-4 rounded-lg shadow-lg">
+                {/* 프로젝트 내용 추가 */}
+              </div>
+            </BlurFade>
+          ))}
         </div>
       </section>
-      <section id="contact">
-        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
-          <BlurFade delay={BLUR_FADE_DELAY * 16}>
-            <div className="space-y-3">
-              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                Contact
-              </div>
-              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Get in Touch
-              </h2>
+
+      {/* Contact Section */}
+      <section
+        id="contact"
+        className="w-full max-w-3xl mx-auto text-center py-12"
+      >
+        <BlurFade delay={BLUR_FADE_DELAY * 16}>
+          <div className="space-y-4">
+            <div className="inline-block bg-foreground text-background px-3 py-1 text-sm rounded-lg">
+              Contact
             </div>
-          </BlurFade>
-        </div>
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">
+              Get in Touch
+            </h2>
+          </div>
+        </BlurFade>
       </section>
     </main>
   );
--- src/components/resume-card.tsx
+++ src/components/resume-card.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { Badge } from "./ui/badge";
 import { Card, CardHeader } from "./ui/card";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
@@ -13,6 +11,8 @@ interface ResumeCardProps {
   logoUrl?: string;
   altText?: string;
   title: string;
+  company?: string;
+  school?: string;
   subtitle?: string;
   href?: string;
   badges?: readonly string[];
@@ -21,12 +21,10 @@ interface ResumeCardProps {
 }
 
 export const ResumeCard = ({
-  logoUrl,
-  altText,
   title,
-  subtitle,
+  company,
+  school,
   href,
-  badges,
   period,
   description,
 }: ResumeCardProps) => {
@@ -45,47 +43,23 @@ export const ResumeCard = ({
       className="block cursor-pointer"
       onClick={handleClick}
     >
-      <Card className="flex">
-        <div className="flex-none">
-          <Avatar className="border size-12 m-auto bg-muted-background dark:bg-foreground">
-            <AvatarImage
-              src={logoUrl}
-              alt={altText}
-              className="object-contain"
-            />
-            <AvatarFallback>{altText?.[0]}</AvatarFallback>
-          </Avatar>
-        </div>
+      <Card className="flex w-full max-w-4xl p-10">
         <div className="flex-grow ml-4 items-center flex-col group">
           <CardHeader>
-            <div className="flex items-center justify-between gap-x-2 text-base">
-              <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
-                {title}
-                {badges && (
-                  <span className="inline-flex gap-x-1">
-                    {badges.map((badge, index) => (
-                      <Badge
-                        variant="secondary"
-                        className="align-middle text-xs"
-                        key={index}
-                      >
-                        {badge}
-                      </Badge>
-                    ))}
-                  </span>
-                )}
+            <div className="flex items-center justify-between gap-x-2 text-xl">
+              <h2 className="inline-flex items-center justify-center font-semibold leading-none text-xl sm:text-xl">
+                {company || school}
                 <ChevronRightIcon
                   className={cn(
                     "size-4 translate-x-0 transform opacity-0 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:opacity-100",
                     isExpanded ? "rotate-90" : "rotate-0"
                   )}
                 />
-              </h3>
-              <div className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right">
+              </h2>
+              <div className="text-2xl sm:text-xl tabular-nums text-muted-foreground text-right">
                 {period}
               </div>
             </div>
-            {subtitle && <div className="font-sans text-xs">{subtitle}</div>}
           </CardHeader>
           {description && (
             <motion.div
