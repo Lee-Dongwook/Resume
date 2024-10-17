dfb567ead23d057d94b855f8d09eb6cc16af281d
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-17 17:52:03 +0900

    feat : Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,45 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import Navbar from "@/components/navbar";
+import { ThemeProvider } from "@/components/theme-provider";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Data } from "@/data/resume";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
 
-import '@/styles/reset.css';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-  title: '이동욱의 이력서',
-  description: '이동욱 이력서입니다.',
-  keywords: 'Resume',
-  publisher: '이동욱',
-  creator: '이동욱',
+  title: {
+    default: Data.name,
+    template: `%s | ${Data.name}`,
+  },
+  description: Data.description,
+  openGraph: {
+    title: `${Data.name}`,
+    description: Data.description,
+    siteName: `${Data.name}`,
+    locale: "ko_KR",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  verification: {
+    google: "",
+    yandex: "",
+  },
 };
 
 export default function RootLayout({
@@ -28,8 +48,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="en">
+      <body
+        className={
+          (cn(
+            "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6"
+          ),
+          fontSans.variable)
+        }
+      >
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <TooltipProvider delayDuration={0}>
+            {children}
+            <Navbar />
+          </TooltipProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
