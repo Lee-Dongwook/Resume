56744e54f1efbf84e239262e39537d7955327369
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-17 17:47:12 +0900

    feat: Navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import { Dock, DockIcon } from "./ui/dock";
+import { ModeToggle } from "./mode-toggle";
+import { buttonVariants } from "./ui/button";
+import { Separator } from "./ui/separator";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { Data } from "@/data/resume";
+import { cn } from "@/lib/utils";
+
+export default function Navbar() {
+  return (
+    <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14">
+      <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg dark:bg-background">
+        <Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-ful items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,0.1)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
+          {Data.navbar.map((item) => (
+            <DockIcon key={item.href}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href={item.href}
+                    className={cn(
+                      buttonVariants({ variant: "ghost", size: "icon" }),
+                      "size-10"
+                    )}
+                  />
+                  <item.icon className="size-4" />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{item.label}</p>
+                </TooltipContent>
+              </Tooltip>
+            </DockIcon>
+          ))}
+          <Separator orientation="vertical" className="h-full" />
+          {Object.entries(Data.contact.social)
+            .filter(([_, social]) => social.navbar)
+            .map(([name, social]) => (
+              <DockIcon key={name}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Link
+                      href={social.url}
+                      className={cn(
+                        buttonVariants({ variant: "ghost", size: "icon" }),
+                        "size-12"
+                      )}
+                    >
+                      <social.icon className="size-4" />
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{name}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </DockIcon>
+            ))}
+          <Separator orientation="vertical" className="h-full py-2" />
+          <DockIcon>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <ModeToggle />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Theme</p>
+              </TooltipContent>
+            </Tooltip>
+          </DockIcon>
+        </Dock>
+      </div>
+    </div>
+  );
+}
